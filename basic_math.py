13e96ecb70136b4eacca97bda65c1f63ecf489f4
Victor Victor Calixte <victor.victor-calixte@stu.ocsb.ca>
2026-09-16 13:22:14 +0000

    Added basic_math.py

---
+++ basic_math.py
@@ -0,0 +1,14 @@
+import math
+
+
+def main():
+    print("4+6={}:".format(4 + 6))
+    print("9-2={}".format(9 - 2))
+    print("2x5={}".format(2 * 5))
+    print("5/5={}".format(5 / 5))
+    print("5^5={}".format(5 ^ 2))
+    print("√12={}".format(math.sqrt(12)))
+
+
+if __name__ == "__main__":
+    main()
